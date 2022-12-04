f28ed02bd2801c99e2064ac793aa3282f195dcaf
Bailey Segail <bails.seegs@gmail.com>
2022-12-04 22:31:37 +1100

    fix: parallax

--- components/home/Opening.tsx
+++ components/home/Opening.tsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import styles from './Opening.module.scss'
 import Image from 'next/legacy/image'
 import { ArrowDownCircle } from 'react-feather'
 import rocketBackground from '../../public/images/illustrations/rocket-background.svg'
 import rocket from '../../public/images/illustrations/rocket.svg'
 import gravity from '../../public/images/illustrations/gravity.svg'
+import { motion, MotionValue, useScroll, useTransform } from 'framer-motion'
+
+function useParallax(value: MotionValue<number>, distance: number) {
+  return useTransform(value, [0, 1], [0, -distance]);
+}
 
 const Opening: React.FC = () => {
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll()
+  const spaceY = useParallax(scrollYProgress, -100);
+  const rocketY = useParallax(scrollYProgress, 900);
+  const rocketX = useParallax(scrollYProgress, -300);
+  const gravityY = useParallax(scrollYProgress, 100);
+
   return (
     <div className={styles.opening}>
       <div className={styles.container}>
@@ -16,26 +28,48 @@ const Opening: React.FC = () => {
           </p>
           <div className={styles.arrowButton}>
             <p>See projects</p>
-            <ArrowDownCircle className={styles.icon} size={24} />
+            <motion.div
+              animate={{
+                y: [0, 5, 0],
+              }}
+              transition={{ repeatType: 'loop', repeatDelay: 0, repeat: Infinity, delay: 0, duration: 2 }}
+            >
+              <ArrowDownCircle className={styles.icon} size={24} />
+            </motion.div>
           </div>
         </div>
 
-        <div className={styles.openingImage}>
-          <div className={styles.superposition}>
+        <div className={styles.openingImage} ref={ref}>
+          <motion.div className={styles.superposition} style={{ y: spaceY }}>
             <Image
               alt={'Space background'}
               src={rocketBackground}
               width={408}
               height={374}
               className={styles.superposition}
+              priority={true}
             />
-          </div>
-          <div className={styles.superposition}>
-            <Image alt={'Rocket image'} src={rocket} width={408} height={374} className={styles.superposition} />
-          </div>
-          <div className={styles.superposition}>
-            <Image alt={'Gravity'} src={gravity} width={408} height={374} className={styles.superposition} />
-          </div>
+          </motion.div>
+          <motion.div className={styles.superposition} style={{ y: rocketY, x: rocketX }}>
+            <Image
+              alt={'Rocket image'}
+              src={rocket}
+              width={408}
+              height={374}
+              className={styles.superposition}
+              priority={true}
+            />
+          </motion.div>
+          <motion.div className={styles.superposition} style={{ y: gravityY }}>
+            <Image
+              alt={'Gravity'}
+              src={gravity}
+              width={408}
+              height={374}
+              className={styles.superposition}
+              priority={true}
+            />
+          </motion.div>
         </div>
       </div>
     </div>
